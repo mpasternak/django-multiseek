af4a438388658bda06260b290ff78aca1cbad38e
Michal Pasternak <michal.dtz@gmail.com>
2014-10-26 08:32:19 +0100

    Connect JS i18n properly

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -4,10 +4,20 @@ from django.contrib import admin
 
 admin.autodiscover()
 
+js_info_dict = {
+    'domain': 'djangojs',
+    'packages': ('multiseek',),
+}
+
 urlpatterns = patterns(
     '',
     url(r'^$', 'test_app.views.root'),
     url(r'^multiseek/', include('multiseek.urls', namespace='multiseek')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^i18n/$',
+        'django.views.i18n.javascript_catalog',
+        js_info_dict,
+        name="js_i18n_catalog")
 )
