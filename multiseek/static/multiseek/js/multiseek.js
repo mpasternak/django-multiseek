53cf98b7e2e3f51f4afa0a9e336beb814b728b5e
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-11 23:27:43 +0200

    jquery 3 needs that

--- multiseek/static/multiseek/js/multiseek.js
+++ multiseek/static/multiseek/js/multiseek.js
@@ -804,7 +804,7 @@ function saveForm(button) {
             } else
                 alert(err);
         }
-    ).error(function () {
+    ).fail(function () {
             alert(error);
         });
 }
