bb467d04322c78b57b203305b58d339c5ee46caa
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-13 01:32:30 +0200

    initial_data command

--- test_project/test_app/management/commands/initial_data.py
+++ test_project/test_app/management/commands/initial_data.py
@@ -1,2 +1,10 @@
 # -*- encoding: utf-8 -*-
 
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = 'Installs initial_data'
+
+    def handle(self, *args, **options):
+        from test_app.conftest import initial_data
+        initial_data()
