bf7452890a306c11d5a81290576390add487ce43
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-15 00:26:16 +0200

    Use a file instead of memory

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -11,11 +11,12 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': 'multiseek_test_app',
