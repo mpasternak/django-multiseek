3893ff18f5bab07354a7d927524ead5a84eac694
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-26 14:11:10 +0200

    Enable building for Py3k

--- setup.py
+++ setup.py
@@ -90,6 +90,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
