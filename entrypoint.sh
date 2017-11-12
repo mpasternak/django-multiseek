29b94427f928a1364492b32f60810453f1dcc836
Michał Pasternak <michal.dtz@gmail.com>
2017-11-13 00:41:12 +0100

    Fix build, maybe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export HOSTNAME=`hostname`
-export THIS_CONTAINER_IP=`cat /etc/hosts | grep $HOSTNAME | cut  -f 1`
-export DJANGO_LIVE_TEST_SERVER_ADDRESS=$THIS_CONTAINER_IP:9015
-
 export PYTHONPATH=/usr/src/app
 
 exec $*
