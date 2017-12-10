c07bbecd5bb230ec3491310ffa36fe3e8b5876d7
Michał Pasternak <michal.dtz@gmail.com>
2017-12-11 00:33:54 +0100

    Support junit.xml

--- test_project/runtests.sh
+++ test_project/runtests.sh
@@ -10,4 +10,4 @@ export PYTHONIOENCODING=utf_8
 python manage.py compress --force -v0
 python manage.py collectstatic --noinput -v0
 
-py.test --cov=../multiseek test_app/tests.py ../multiseek
+py.test --cov=../multiseek --junitxml=../junit.xml test_app/tests.py ../multiseek
