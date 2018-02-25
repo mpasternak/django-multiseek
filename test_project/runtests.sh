bdd36eaaa907d7bde10f341eff110bf27b6e4e5e
Michał Pasternak <michal.dtz@gmail.com>
2018-02-25 08:20:20 +0100

    Use yarn to install stuff

--- test_project/runtests.sh
+++ test_project/runtests.sh
@@ -7,6 +7,8 @@ export PYTHONPATH=..:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=test_project.settings
 export PYTHONIOENCODING=utf_8
 
+yarn
+
 python manage.py compress --force -v0
 python manage.py collectstatic --noinput -v0
 
