fc18c346c08bdfcc6798f000a7d0b69ba7eae0af
Michał Pasternak <michal.dtz@gmail.com>
2018-06-29 19:38:15 +0200

    Bump version: 0.9.37 → 0.9.38

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ def reqs(f):
         
 setup(
     name='django-multiseek',
-    version='0.9.37',
+    version='0.9.38',
     description='Build a form to seek records using multiple parameters',
     author=u'Michał Pasternak',
     author_email='michal.dtz@gmail.com',
